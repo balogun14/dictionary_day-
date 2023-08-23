913c6b11a799279bce9ef4167f7f7f20c0583c58
balogun14 <awwalbalogun06@gmail.com>
2023-08-23 19:57:44 +0100

    fixed the issue

--- scripts.js
+++ scripts.js
@@ -1,26 +1,40 @@
 const search = document.getElementById("search");
 const container = document.getElementById("results");
+const loader = document.querySelector(".circle");
 // var value = search.value;
 // const API_URl = ;
+const clearResult = document.getElementById("clear");
 var dictionary = [];
 async function SearchWord(value) {
-  let response = await fetch(
-    `https://api.dictionaryapi.dev/api/v2/entries/en/${value}`
-  );
-  let data = await response.json();
-  //   console.log(data[0].meanings);
-  return data[0].meanings;
+  try {
+    let response = await fetch(
+      `https://api.dictionaryapi.dev/api/v2/entries/en/${value}`
+    );
+    let data = await response.json();
+    //   console.log(data[0].meanings);
+    return data[0].meanings;
+  } catch (error) {
+    return null;
+  }
 }
-
+function showLoader() {
+  loader.classList.add("show");
+  setTimeout(() => {
+    loader.classList.remove("show");
+  }, 200);
+}
 async function displayWords(e) {
   const result_element = document.createElement("div");
   if (e != null) {
+    showLoader();
+
     let results = await SearchWord(e);
-    for (let i in results) {
-      const result_element = document.createElement("div");
-      result_element.classList.add("result");
-      results.forEach((meaning) => {
-        result_element.innerHTML = `
+    if (results != null) {
+      for (let i in results) {
+        const result_element = document.createElement("div");
+        result_element.classList.add("result");
+        results.forEach((meaning) => {
+          result_element.innerHTML = `
         <p class="partOfSpeech">
             ${results[i].partOfSpeech}
         </p>
@@ -28,12 +42,19 @@ async function displayWords(e) {
           ${results[i].definitions[0].definition}
         </p>
         `;
-        container.appendChild(result_element);
-      });
+          container.appendChild(result_element);
+        });
+      }
+    } else {
+      container.innerHTML = `
+      <div class='error'>
+      ${e} returned no results
+      </div>
+      `;
     }
   }
 }
-alert("Made by Awwal");
+console.log("Made by Awwal");
 
 // SearchWord();
 window.addEventListener("keydown", (e) => {
@@ -41,7 +62,8 @@ window.addEventListener("keydown", (e) => {
   if (e.keycode === 13 || e.key === "Enter") {
     displayWords(word);
   }
-  if (word == "") {
-    location.reload();
-  }
+});
+
+clearResult.addEventListener("click", (e) => {
+  location.reload();
 });
